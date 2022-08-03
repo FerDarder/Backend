29335a2af3445220a54ee2f00a218550f2e89187
Fernando <fernandodarder01@gmail.com>
2022-08-03 09:45:15 -0300

    Desafío2 con Async/Await

--- desafio2/app.js
+++ desafio2/app.js
@@ -4,78 +4,79 @@ class Contenedor{
     constructor(fileName){
         this.fileName = fileName;
     }
-    save(object){
-        fs.readFile(this.fileName, 'utf-8', (error,contenido)=>{
-            if (error)
-                console.log('Ocurrio un error')
-            else { 
-                let nuevoContenido = JSON.parse(contenido)
-                object.id= nuevoContenido.length + 1;
-                nuevoContenido.push(object)
-                fs.writeFile(this.fileName, JSON.stringify(nuevoContenido), error=>{
-                    if (error)
-                        console.log('Ocurrio un error')
-                    else {
-                        return object.id
-                    }
-                })
-            }
-        })
+    async save(object){
+        try{
+            const array = await fs.readFileSync(this.fileName, 'utf-8')
+            const parsedArray = JSON.parse(array)
+            let existe = false
+            for (let i=0;i<parsedArray.length;i++)
+                if (parsedArray[i].title == object.title)
+                    existe = true
+            if (existe == true)
+                return 'Este producto ya existe'
+                object.id=parsedArray.length + 1
+            parsedArray.push(object)
+            await fs.writeFileSync(this.fileName, JSON.stringify(parsedArray))
+            return object.id;
+        }
+        catch (err){
+            console.log(err)
+        }
     }
-    getById(number){
-        fs.readFile(this.fileName, 'utf-8', (error,contenido)=>{
-            if (error)
-                console.log('Ocurrio un error')
-            else {
-                let nuevoContenido = JSON.parse(contenido)
-                let obj = nuevoContenido.find(obj => obj.id == number);
-                console.log(obj);
-            }
-            })
+
+
+    async getAll(id){
+        const array = await fs.readFileSync(this.fileName, 'utf-8')
+        return JSON.parse(array)
     }
-    getAll(){
-        fs.readFile(this.fileName, 'utf-8', (error,contenido)=>{
-            if (error)
-                console.log('Ocurrio un error')
-            else 
-                console.log(JSON.parse(contenido));
-        })
+
+    async getById(id){
+        const array = await fs.readFileSync(this.fileName, 'utf-8')
+        const parsedArray = JSON.parse(array)
+        const obj=parsedArray.find(obj => obj.id == id)
+        return obj;
     }
-    deleteById(number){
-        fs.readFile(this.fileName, 'utf-8', (error,contenido)=>{
-            if (error)
-                console.log('Ocurrio un error')
-            else {
-                let nuevoContenido = JSON.parse(contenido)
-                const obj = nuevoContenido.find(obj=>obj.id == number)
-                const index = nuevoContenido.indexOf(obj)
-                nuevoContenido.splice(index, 1)
-                fs.writeFile(this.fileName, JSON.stringify(nuevoContenido), error=>{
-                    if (error)
-                        console.log('Ocurrio un error')
-                })
-            }
-        })
+
+    async deleteById(id){
+        const array = await fs.readFileSync(this.fileName, 'utf8');
+        const parsedArray = JSON.parse(array);
+        const obj = parsedArray.find(obj => obj.id == id);
+        const index = parsedArray.indexOf(obj);
+        parsedArray.splice(index, 1);
+        await fs.writeFileSync(this.fileName, JSON.stringify(parsedArray));
     }
-    deleteAll(){
-        fs.writeFile(this.fileName, JSON.stringify([]), error=>{
-            if (error)
-                console.log('Ocurrio un error')
-            else 
-                console.log('Productos eliminados')
-        })
+
+    async deleteAll(){
+        await fs.writeFileSync(this.fileName, JSON.stringify([]))
     }
 }
 
-const manejador = new Contenedor('./productos.txt');
+
 const productoNuevo = {
     "title": 'Mate',                                                                                                                                 
     "price": 954.74, 
     "thumbnail": "https://cdn0.iconfinder.com/data/icons/drinks-22/512/drink_water_bottle_beverage-34-512.png"
 }
 
-// manejador.save(productoNuevo);
-// manejador.getById(2)
-manejador.getAll()
-// manejador.deleteById(4)
-// manejador.deleteAll()
+const instance = new Contenedor('productos.txt');
+
+const main = async () => {
+    const test = await instance.save(productoNuevo);
+    console.log(test);
+    
+    const productos = await instance.getAll();
+    productos.forEach(product => console.log(product.title, product.price, product.thumbnail));
+    
+    const product = await instance.getById(3);
+    console.log(product);
+
+    await instance.deleteById(1);
+    const productos2 = await instance.getAll();
+    productos2.forEach(product => console.log(product.title, product.price));
+
+    // Una vez ejecutado el próximo método, se eliminan todos los productos del archivo.txt
+    // await instance.deleteAll();
+
+}
+
+main();
